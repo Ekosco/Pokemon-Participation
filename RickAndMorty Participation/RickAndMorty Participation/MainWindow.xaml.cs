9ab814290f95fe95c334b3d1e4f750765c3e5ec4
Lizzy Kosco <Ekosco@ou.edu>
2021-03-25 00:32:18 -0500

    Completed Rick and Morty Participation

---
+++ RickAndMorty Participation/RickAndMorty Participation/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace RickAndMorty_Participation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RickAndMortyAPI api;
+            string url = "https://rickandmortyapi.com/api/character";
+            /*while(string.IsNullOrEmpty(url) == false)
+            {
+                using (var client = new HttpClient())
+                {
+                    var json = client.GetStringAsync(url).Result;
+                    api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+                }
+
+                foreach (Character item in api.results.OrderBy(x => x.name).ToList())
+                {
+                    characterLstBx.Items.Add(item);
+                }
+
+                url = api.info.next;*/
+            //All you add is while loop and url statement for scrolling through all pages within max api display limit
+            while (string.IsNullOrEmpty(url) == false)
+            {
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    //used to validate if there is an error or not: Response and if statement
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = response.Content.ReadAsStringAsync().Result;
+                        api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+                        foreach (Character item in api.results)
+                        {
+                            characterLstBx.Items.Add(item);
+                        }
+
+                        url = api.info.next;
+                    }
+                }
+            }
+        }
+
+        private void characterLstBx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCharacter = (Character)characterLstBx.SelectedItem;
+            characterImg.Source = new BitmapImage(new Uri(selectedCharacter.image));
+        }
+    }
+}
---
+++ RickAndMorty Participation/RickAndMorty Participation/RickAndMortyAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RickAndMorty_Participation
+{
+    public class RickAndMortyAPI
+    {
+        public Info info { get; set; }
+        public List<Character> results;
+    }
+
+    public class Character
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+    }
+}
